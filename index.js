30e2bc079cd33b49a080228e01b76dd54cde43ad
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-03-08 19:00:39 +0530

    will submit the results to the constructor

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-console.log("s")
 let myLibrary = [];
-
-function Book() {
-  // the constructor...
+let temp
+function Book(title,author,pages,read) {
+  this.author=author
+  this.title=title 
+  this.pages=pages 
+  this.read=read
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  title =document.querySelector(".title").value
+  author=document.querySelector(".author").value
+  pages=document.querySelector(".pages").value
+  read=document.querySelector(".read").checked
+  let newBook= new Book(title,author,pages,read)
+  console.log(newBook)  
+
 }
+document.querySelector(".new-book").addEventListener("click",function(){
+  let book= document.querySelector(".details");
+  book.style.display='flex';
+}
+)
+document.querySelector(".add-button").addEventListener("click",function(event){
+  event.preventDefault()  
+  console.log(event)
+  addBookToLibrary()}
+  )
