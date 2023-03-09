b775486a54b737d2d58e60b1ae74f80f9a446364
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-03-09 18:09:33 +0530

    allows user to toggle read button in the library

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ function Book(title,author,pages,read) {
   this.pages=pages 
   this.read=read
 }
+function toggleRead(i){
+  
+  if(myLibrary[i].read==true){ 
+       myLibrary[i].read=false
+  }
+  else if(myLibrary[i].read==false){
+    myLibrary[i].read=true
+  }
+  render();
+}
+function removeBook(index){
+  myLibrary.splice(index,1)
+  console.log(index)
+  render();
+  }
 function render(){
   let book= document.querySelector(".render-cards")
   book.innerHTML=''
@@ -19,17 +34,13 @@ function render(){
         <h5 class="card-title">${bookOb.pages }</h5>
         <div style="display:flex;justify-content:space-between;">
           <button style="margin:10px 15px" class="btn btn-secondary"onclick="removeBook(${i})">Remove</button>
-          <a href="#" style="margin:10px 15px" class="btn btn-primary"  ">Read</a>
+          <button style="margin:10px 15px;width:100px" class="btn btn-primary"  onclick="toggleRead(${i})">${bookOb.read?'Read':'Not Read'}</Button>
         </div>
       </div>`
     book.appendChild(bookEl)
   }
 }
-function removeBook(index){
-myLibrary.splice(index,1)
-console.log(index)
-render();
-}
+
 function addBookToLibrary() {
   title =document.querySelector(".title").value
   author=document.querySelector(".author").value
